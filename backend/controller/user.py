d4818d7cd6b3da44688737644cc1244850bfbb0a
Victor Maricato <victormaricato@cloud.upwork.com>
2023-07-10 15:44:01 -0300

    find notes from mongo

--- backend/controller/user.py
+++ backend/controller/user.py
@@ -1,5 +1,6 @@
 from sqlalchemy.sql.functions import user
 from backend.repository.user import UserRepository
+from backend.repository.notes import NotesRepository
 from backend.repository.session import SessionRepository
 from backend.models.user import NumberInsert
 from fastapi import APIRouter, Depends, Header
@@ -11,15 +12,18 @@ router = APIRouter(prefix="/user")
 async def get(user_id: str,
 authorization: str = Header(None),
 user_repository: UserRepository = Depends(),
+notes_respository: NotesRepository = Depends(),
 session_repository: SessionRepository = Depends()
 ):
     if(session_repository.validate(authorization, user_id)):
-        return user_repository.find_by_id(user_id)
+        user = user_repository.find_by_id(user_id)
+        user.notes = notes_respository.find_by_user_id(user_id)
+        return user
     return dict(error="Invalid token.")
 
 @router.post("/number")
 async def set_number(
-number_insert: NumberInsert, 
+number_insert: NumberInsert,
 user_id: str = Header(None, convert_underscores=False),
 authorization: str = Header(None),
 user_repository: UserRepository = Depends(),
--- backend/repository/notes.py
+++ backend/repository/notes.py
@@ -8,3 +8,7 @@ class NotesRepository(BaseMongoRepository):
     def create(self, note: NoteModel) -> NoteModel:
         result = self.insert_and_return(note.dict(by_alias=True))
         return NoteModel(**result)
+
+    def find_by_user_id(self, user_id: str) -> list[NoteModel]:
+        result = self.collection.find({"user_id": user_id})
+        return [NoteModel(**note) for note in result]
