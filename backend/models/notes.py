e8ec2bf6b694d5901ab751108cd24e4eaf1fafdd
Victor Maricato <victormaricato@cloud.upwork.com>
2023-07-10 02:54:38 -0300

    fix: mongo UUID representation

--- backend/models/notes.py
+++ backend/models/notes.py
@@ -1,21 +1,15 @@
 from typing import Optional
 
 from bson.objectid import ObjectId
+from uuid import UUID
 from pydantic import BaseModel, Field
 
 
 class NoteModel(BaseModel):
-    id: Optional[ObjectId] = Field(alias='_id')
+    id: UUID
     tweet_id: Optional[str]
     user_id: str
     content: str
-
-    class Config:
-        allow_population_by_field_name = True
-        arbitrary_types_allowed = True
-        json_encoders = {
-            ObjectId: str
-        }
 
 
 class NoteInsert(BaseModel):
